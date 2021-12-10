f73d0e39bac196c6345ceaf8a1af6b8d8a75f6e1
IamWilly123 <95930069+IamWilly123@users.noreply.github.com>
2021-12-10 11:34:55 -0500

    Update Willy.R
    Made some changes
    Felling cute today

--- Willy.R
+++ Willy.R
@@ -1,11 +1,11 @@
-Xbar <- numeric(10000)
-#set.seed(300)
-for (i in 1:10000)
+Xbar <- numeric(10)
+#set.seed(3)
+for (i in 1:10)
 {
-  x <- rexp(1000, rate = 1/15)
+  x <- rexp(100, rate = 1/7)
   Xbar[i] <- mean(x)
 }
-hist(Xbar, col= "yellow", ylim=c(0,1),main="Simulated sampling distribution", xlab="means", freq=FALSE)
-lines(10:20, dnorm(10:20, mean(Xbar), sd(Xbar) ), col="orange",type = "b") #overlay pmf for Normal(17)
+hist(Xbar, col= "cyan", ylim=c(0,1),main="Simulated sampling distribution", xlab="means", freq=FALSE)
+lines(10:20, dnorm(10:20, mean(Xbar), sd(Xbar) ), col="light green",type = "b") #overlay pmf for Normal(17)
 
 Dev.off()
